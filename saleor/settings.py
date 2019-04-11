6e7845b63c6756b7bfa18011fd2d86e4505230d9
marqpdx <marqpdx@gmail.com>
2019-04-11 00:33:53 -0700

    rmd skey from settings, yay

--- saleor/settings.py
+++ saleor/settings.py
@@ -175,7 +175,7 @@ TEMPLATES = [{
         'string_if_invalid': '<< MISSING VARIABLE "%s" >>' if DEBUG else ''}}]
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = os.environ.get('SECRET_KEY', 'asdfasdfasdf99')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
